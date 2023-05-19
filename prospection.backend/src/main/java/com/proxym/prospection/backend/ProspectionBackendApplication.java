ca60dc3b37ee6afaaf185c61218927073b61791a
sawssen.ammar <sawssen.ammar@proxym-it.com>
2023-05-19 13:15:57 +0100

    [ADD] Adding new repo

---
+++ prospection.backend/src/main/java/com/proxym/prospection/backend/ProspectionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.proxym.prospection.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProspectionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProspectionBackendApplication.class, args);
+	}
+
+}
---
+++ prospection.backend/src/test/java/com/proxym/prospection/backend/ProspectionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proxym.prospection.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProspectionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
