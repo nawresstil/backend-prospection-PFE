358620dca208c28204cd2944f6405ba9b2994946
nawresstil <nawresstilouch1@gmail.com>
2023-05-21 01:08:17 +0200

    fix

--- prospection.backend/src/main/java/com/proxym/prospection/backend/auth/dao/RegisterRequest.java
+++ prospection.backend/src/main/java/com/proxym/prospection/backend/auth/dao/RegisterRequest.java
@@ -14,9 +14,14 @@ public class RegisterRequest {
 
     private String firstname;
     private String lastname;
-    private String username;
-    private String password;
-    private String email;
-    private Number phone;
+    private String joiningDate;
     private Role role;
+    private String Departments;
+    private String username;
+    private String email;
+    private String password;
+    private String confirmPassword;
+    private Number phone;
+
+
 }
